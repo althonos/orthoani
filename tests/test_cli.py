2cb5d7e2e7277e6aac5f0af01ae213dcc32673db
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-06-15 23:26:29 +0200

    Add small integration tests for API and CLI

---
+++ tests/test_cli.py
@@ -0,0 +1,25 @@
+import os
+import glob
+import pathlib
+import unittest
+import subprocess
+import sys
+
+from Bio.SeqIO import read
+
+import orthoani
+
+
+class TestCli(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data = pathlib.Path(__file__).parent / "data"
+        cls.p1, cls.p2 = map(os.fspath, cls.data.glob("*.fna"))
+
+    def test_orthoani_cli(self):
+        # check the score we get is the same as the OrthoANI Java implementation
+        args = [sys.executable, "-m", "orthoani", "-q", self.p1, "-r", self.p2]
+        proc = subprocess.run(args, capture_output=True, cwd=self.data.parent.parent)
+        proc.check_returncode()
+        self.assertEqual(proc.stdout, b"0.5725\n")
---
+++ tests/test_orthoani.py
@@ -0,0 +1,28 @@
+import os
+import glob
+import pathlib
+import unittest
+
+from Bio.SeqIO import read
+
+import orthoani
+
+
+class TestOrthoani(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data = pathlib.Path(__file__).parent / "data"
+        cls.r1, cls.r2 = map(lambda p: read(p, "fasta"), cls.data.glob("*.fna"))
+
+    def test_orthoani_single(self):
+        # check the score we get is the same as the OrthoANI Java implementation
+        ani = orthoani.orthoani(self.r1, self.r2)
+        self.assertAlmostEqual(ani, 0.5725)
+
+    def test_orthoani_pairwise(self):
+        mapping = orthoani.orthoani_pairwise([self.r1, self.r2])
+        self.assertAlmostEqual(mapping[self.r1.id, self.r2.id], 0.5725)
+        self.assertAlmostEqual(mapping[self.r2.id, self.r1.id], 0.5725)
+        self.assertAlmostEqual(mapping[self.r1.id, self.r1.id], 1.0)
+        self.assertAlmostEqual(mapping[self.r2.id, self.r2.id], 1.0)
