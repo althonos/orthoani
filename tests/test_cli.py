e1af2875dc4b80571ce3f802937793e6484fc547
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-06-16 12:45:24 +0200

    Add tests for CLI to verify return code of failing commands

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,6 @@ import pathlib
 import unittest
 import subprocess
 import sys
-from subprocess import PIPE
 
 from Bio.SeqIO import read
 
@@ -17,9 +16,35 @@ class TestCli(unittest.TestCase):
         cls.data = pathlib.Path(__file__).parent / "data"
         cls.p1, cls.p2 = map(os.fspath, cls.data.glob("*.fna"))
 
+    def shell(self, args):
+        return subprocess.run(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            cwd=self.data.parent.parent,
+        )
+
     def test_orthoani_cli(self):
         # check the score we get is the same as the OrthoANI Java implementation
         args = [sys.executable, "-m", "orthoani", "-q", self.p1, "-r", self.p2]
-        proc = subprocess.run(args, stdout=PIPE, cwd=self.data.parent.parent)
+        proc = self.shell(args)
         proc.check_returncode()
         self.assertEqual(proc.stdout, b"0.5725\n")
+
+    def test_return_code(self):
+        args = [sys.executable, "-m", "orthoani", "-q", self.data, "-r", self.p2]
+        proc = self.shell(args)
+        self.assertEqual(proc.returncode, 21) # IsADirectoryError
+
+        args = [sys.executable, "-m", "orthoani", "-q", "_", "-r", self.p2]
+        proc = self.shell(args)
+        self.assertEqual(proc.returncode, 2) # FileNotFoundError
+
+    def test_return_code_traceback(self):
+        args = [sys.executable, "-m", "orthoani", "-T", "-q", self.data, "-r", self.p2]
+        proc = self.shell(args)
+        self.assertEqual(proc.returncode, 21) # IsADirectoryError
+
+        args = [sys.executable, "-m", "orthoani", "-T", "-q", "_", "-r", self.p2]
+        proc = self.shell(args)
+        self.assertEqual(proc.returncode, 2) # FileNotFoundError
