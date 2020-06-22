4a1c73339ea9ec26bed78a94eb4004d23f2d8e51
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-06-22 13:27:09 +0200

    Add test checking we chop multi-contigs sequences like the Java implementation

--- tests/test_orthoani.py
+++ tests/test_orthoani.py
@@ -2,8 +2,10 @@ import os
 import glob
 import pathlib
 import unittest
+import tempfile
 
-from Bio.SeqIO import read
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqIO import read, parse
 
 import orthoani
 
@@ -12,7 +14,7 @@ class TestOrthoani(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.data = pathlib.Path(__file__).parent / "data"
-        cls.r1, cls.r2 = map(lambda p: read(p, "fasta"), cls.data.glob("*.fna"))
+        cls.r1, cls.r2 = map(lambda p: read(p, "fasta"), cls.data.glob("1852379.*.fna"))
 
     def test_orthoani_single(self):
         # check the score we get is the same as the OrthoANI Java implementation
@@ -25,3 +27,30 @@ class TestOrthoani(unittest.TestCase):
         self.assertAlmostEqual(mapping[self.r2.id, self.r1.id], 0.5725)
         self.assertAlmostEqual(mapping[self.r1.id, self.r1.id], 1.0)
         self.assertAlmostEqual(mapping[self.r2.id, self.r2.id], 1.0)
+
+
+class TestChop(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data = pathlib.Path(__file__).parent / "data"
+
+    def test_single_contig(self):
+        record = read(self.data / "U00096-3.fasta", "fasta")
+        with (self.data / "U00096-3.fasta.chopped.fasta").open() as f:
+            expected = list(parse(f, "fasta"))
+        with tempfile.NamedTemporaryFile(mode="rt", suffix=".fna") as tmp:
+            orthoani._chop(record, tmp.name, 1020)
+            actual = list(parse(tmp, "fasta"))
+        for actual_record, expected_record in zip(actual, expected):
+            self.assertEqual(actual_record.seq, expected_record.seq)
+
+    def test_multiple_contig(self):
+        record = parse(self.data / "NZ_AAEN01000029.fna", "fasta")
+        with (self.data / "NZ_AAEN01000029.fna.chopped.fasta").open() as f:
+            expected = list(parse(f, "fasta"))
+        with tempfile.NamedTemporaryFile(mode="rt", suffix=".fna") as tmp:
+            orthoani._chop(record, tmp.name, 1020)
+            actual = list(parse(tmp, "fasta"))
+        for actual_record, expected_record in zip(actual, expected):
+            self.assertEqual(actual_record.seq, expected_record.seq)
