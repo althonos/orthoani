4d97dba7c701b06c01bdc1ec62076379f68d0e19
Martin Larralde <martin.larralde@embl.de>
2025-02-21 16:35:40 +0100

    Rename module from `orthoani` to `pyorthoani`

--- tests/test_orthoani.py
+++ tests/test_orthoani.py
@@ -10,7 +10,7 @@ from os import fspath
 from Bio.SeqRecord import SeqRecord
 from Bio.SeqIO import read, parse
 
-import orthoani
+import pyorthoani
 
 
 class TestOrthoani(unittest.TestCase):
@@ -33,12 +33,12 @@ class TestOrthoani(unittest.TestCase):
 
     def test_orthoani_single(self):
         # check the score we get is the same as the OrthoANI Java implementation
-        ani = orthoani.orthoani(self.r1, self.r2)
+        ani = pyorthoani.orthoani(self.r1, self.r2)
         self.assertAlmostEqual(ani, 0.5725)
         self.assertEmpty(os.listdir(self.sandbox))
 
     def test_orthoani_pairwise(self):
-        mapping = orthoani.orthoani_pairwise([self.r1, self.r2])
+        mapping = pyorthoani.orthoani_pairwise([self.r1, self.r2])
         self.assertAlmostEqual(mapping[self.r1.id, self.r2.id], 0.5725)
         self.assertAlmostEqual(mapping[self.r2.id, self.r1.id], 0.5725)
         self.assertAlmostEqual(mapping[self.r1.id, self.r1.id], 1.0)
@@ -57,7 +57,7 @@ class TestChop(unittest.TestCase):
         with (self.data / "U00096-3.fasta.chopped.fasta").open() as f:
             expected = list(parse(f, "fasta"))
         with tempfile.NamedTemporaryFile(mode="rt", suffix=".fna") as tmp:
-            orthoani._chop(record, tmp.name, 1020)
+            pyorthoani._chop(record, tmp.name, 1020)
             actual = list(parse(tmp.file, "fasta"))
         for actual_record, expected_record in zip(actual, expected):
             self.assertEqual(actual_record.seq, expected_record.seq)
@@ -68,7 +68,7 @@ class TestChop(unittest.TestCase):
         with (self.data / "NZ_AAEN01000029.fna.chopped.fasta").open() as f:
             expected = list(parse(f, "fasta"))
         with tempfile.NamedTemporaryFile(mode="rt", suffix=".fna") as tmp:
-            orthoani._chop(record, tmp.name, 1020)
+            pyorthoani._chop(record, tmp.name, 1020)
             actual = list(parse(tmp.file, "fasta"))
         for actual_record, expected_record in zip(actual, expected):
             self.assertEqual(actual_record.seq, expected_record.seq)
